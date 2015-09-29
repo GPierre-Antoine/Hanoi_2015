f07bbf0965e7bed1005a35a812b5ae860e0ba12f
jikar <julienjandot@orange.fr>
2015-09-29 08:29:27 +0200

    cc

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
+namespace {
+
+    void Affichage(){
+        sf::Window hanoïWindow(sf::VideoMode(800, 600), "Hanoï", sf::Style::Default);
+
+        glEnable(GL_TEXTURE_3D);
+
+        while (hanoïWindow.isOpen){
+            sf::Event gameEvent;
+            while (hanoïWindow.pollEvent(gameEvent)){
+                if (gameEvent.type == sf::Event::Closed) hanoïWindow.close();
+            }
+        }
+    }//Affichage
+
+}//namespace
+
+
+
 int main()
 {
     cout << "coucou";
