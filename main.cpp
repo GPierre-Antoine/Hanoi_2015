12854d9d939d43c09a6f10860c6fbe36be45ed3b
j14003626 <theo.jeanjean42@gmail.com>
2015-09-29 08:54:37 +0200

    Merge branch 'master' of /home/j14003626/Bureau/Hanoi_2015 with conflicts.

--- main.cpp
+++ main.cpp
@@ -2,35 +2,14 @@
 // main.cpp
 //
 
-#include <iostream>
-#include "HanoiCube.h"
+#include "HanoiCuboid.h"
 
 using namespace nsHanoi;
-using namespace std;
-
-namespace {
-
-    void Affichage(){
-        sf::Window hanoïWindow(sf::VideoMode(800, 600), "Hanoï", sf::Style::Default);
-
-        glEnable(GL_TEXTURE_3D);
-
-        while (hanoïWindow.isOpen){
-            sf::Event gameEvent;
-            while (hanoïWindow.pollEvent(gameEvent)){
-                if (gameEvent.type == sf::Event::Closed) hanoïWindow.close();
-            }
-        }
-    }//Affichage
-
-}//namespace
-
-
 
 int main()
 {
-    HanoiCube Cube = HanoiCube(HanoiPoint(0, 0, 0), HanoiPoint(1, 0, 0),
-                               HanoiPoint(0, 0, -1), HanoiPoint(0, 0.5, 0));
-    Cube.Move(1, 1, 1);
+    HanoiCuboid Pave = HanoiCuboid(HanoiPoint(0, 0, 0), HanoiPoint(1, 0, 0),
+                                   HanoiPoint(0, 0, -1), HanoiPoint(0, 0.5, 0));
+    Pave.Move(1, 1, 1);
     return 0;
 }
